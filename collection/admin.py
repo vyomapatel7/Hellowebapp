3f999a14a25f0c2831d313544031a80e4d2e502b
vyomapatel7 <vyoma.patel07@gmail.com>
2019-04-28 11:03:46 +0200

    Chapter 6 Database

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+# import your model
+from collection.models import Profile
+
+# set up automated slug creation
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
+# and register it
+admin.site.register(Profile, ProfileAdmin)
