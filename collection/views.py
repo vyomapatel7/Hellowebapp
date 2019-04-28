bdc3c99248df352e4216ccdb9342a8f619ac8896
vyomapatel7 <vyoma.patel07@gmail.com>
2019-04-28 06:40:12 +0200

    how do i title this commit

--- collection/views.py
+++ collection/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 # this is your new view
-return render(request, 'index.html')
+    return render(request, 'index.html')
--- mysite/urls.py
+++ mysite/urls.py
@@ -13,17 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import path
+
 from django.contrib import admin
 from django.views.generic import TemplateView
 from collection import views
+from django.urls import include, path
+
 urlpatterns = [
-path('', views.index, name='home'),
-path('about/',
-TemplateView.as_view(template_name='about.html'),
+    path('', views.index, name='home'),
+    path('about/',
+    TemplateView.as_view(template_name='about.html'),
 name='about'),
-path('contact/',
-TemplateView.as_view(template_name='contact.html'),
-name='contact'),
-path('admin/', admin.site.urls),
+    path('contact/',
+    TemplateView.as_view(template_name='contact.html'),
+    name='contact'),
+    path('admin/', admin.site.urls),
 ]
